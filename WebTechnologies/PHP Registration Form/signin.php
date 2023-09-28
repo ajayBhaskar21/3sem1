c01894a8c99ad4c7c3af83cf14db917582d698d1
Broken <124747342+ajayBhaskar21@users.noreply.github.com>
2023-09-28 21:19:54 +0530

    Updated

--- WebTechnologies/PHP Registration Form/signin.php
+++ WebTechnologies/PHP Registration Form/signin.php
@@ -4,10 +4,8 @@
     $username = "root";
     $password = "root";
     $dbname = "mydb";
-
     // Create a connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
-
     // Check the connection
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
