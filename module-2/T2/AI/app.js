e7a218bb66418e8b9d75b8aefe9171b08cf3c56e
Broken <124747342+ajayBhaskar21@users.noreply.github.com>
2023-11-14 11:42:57 +0530

    Add files via upload

--- module-2/T2/AI/app.js
+++ module-2/T2/AI/app.js
@@ -1,19 +1,20 @@
-
 function observes () {
-
-
     // child watches father walking
-
+    console.log('infant observing the surroundings!!!');
+    description.innerText = 'infant observing the surroundings!!!';
 }
 
 function fatherSupports () {
     // father holds child and makes him to walk
-
+    console.log('father supports infant to walk');
+    description.innerText = 'father supports infant to walk';
 }
 
 function fatherTrainsInfant() {
     // count 
     countFatherTrains++;
+    console.log('count of Father trains = ' + countFatherTrains);
+    description.innerText = `count of Father trains = ${countFatherTrains}`;
 }
 
 function fatherCalls() {
@@ -25,48 +26,88 @@ function fatherCalls() {
 }
 
 function ageChanger() {
-    let ages = [];
-    for (let i = 0; i <= 15; i ++) 
-        ages.push(i);
-    
+    let ages = [3, 6, 9, 12, 15, 18];
     // set age to random number from ages array
     age = ages[Math.floor(Math.random() * ages.length)];
+    console.log('current age is ' + age);
+    description.innerText = `current age is ${age}`;
 }
 
 function trysToCrawl() {
-
-    if (countFatherTrains > 3 && fatherCalls() && (age >= 6 && age <= 9)) {
-        console.log('image showing an infant crawls');
+    let hasFatherCalled = fatherCalls();
+    if (!hasFatherCalled) {
+        console.log('father has not called');
+        description.innerText = 'father has not called';
+    } 
+    else {
+        if (countFatherTrains > 3 && (age >= 6 && age <= 9)) {
+            console.log('image showing an infant trying to crawl');
+            description.innerText = 'image showing an infant trying to crawl';
+        }
+        else {
+            console.log('still learning!!!');
+            description.innerText = 'still learning!!!';
+        }
     }
-
 }
 
 function crawls() {
-
-    if (countFatherTrains > 7 && fatherCalls() && (age >= 10 && age <= 11)) {
-        console.log('infant crawls');
+    let hasFatherCalled = fatherCalls();
+    if (!hasFatherCalled) {
+        console.log('father has not called');
+        description.innerText = 'father has not called';
     }
-
+    else {
+        if (countFatherTrains > 7 &&  (age >= 10 && age <= 11)) {
+            console.log('infant crawls');
+            description.innerText = 'infant crawls';
+        }
+        else {
+            console.log('still learning!!!');
+            description.innerText = 'still learning!!!';
+        }
+    }
 }
 
 function trysToStand() {
-
-    if (fatherCalls() && (age >= 12 && age <= 15)) {
-        // count ++;
-    }
-    return count;
+    countChildTrysToStand++;
+    console.log('trying to stand!!!');
+    description.innerText = 'trying to stand!!!';
 }
 
 function stands () {
-
-    if (trysToStand() > 5) {
+    if (countChildTrysToStand > 5 && (age >= 10 && age <= 15)) {
         // stands successfully
+        console.log('child stands');
+        description.innerText = 'child stands';
     }
-
+    else {
+        console.log('child still learning!!!!');
+        description.innerText = 'child still learning!!!!';
+    }
 }
 
+function trysToWalk() {
+    coundChildTrysToWalk++;
+    console.log('trying to walking!!');
+    description.innerText = 'trying to walking!!';
+}
 
+function walks() {
+    if (coundChildTrysToWalk > 6 && (age > 15 && age < 20)) {
+        console.log('child walks');
+        description.innerText = 'child walks';
+    }
+    else {
+        console.log('child still learning!!!!');
+        description.innerText = 'child still learning!!!!';
+    }
+}
 
 let countFatherTrains = 0;
+let countChildTrysToStand = 0;
+let coundChildTrysToWalk = 0;
 let age = 0;
+let description = document.getElementById('description');
+
 
